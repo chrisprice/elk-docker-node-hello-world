54586f9e7c49d9b3204c49ba2c3fe9369de480b2
Chris Price <price.c@gmail.com>
2015-08-20 11:52:29 +0100

    Add build script

---
+++ build-all.sh
@@ -0,0 +1,7 @@
+docker build -t chrisprice/monitoring-node-datastore node-datastore/.
+
+docker build -t chrisprice/monitoring-logstash-datastore logstash-datastore/.
+
+docker build -t chrisprice/monitoring-node-service node-service/.
+
+docker build -t chrisprice/monitoring-logstash-service logstash-service/.
